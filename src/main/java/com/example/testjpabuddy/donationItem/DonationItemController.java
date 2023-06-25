3c15a093f0f6d17656925a2bdd9334d875b6af59
willboesetrio <95431444+willboesetrio@users.noreply.github.com>
2023-06-25 17:47:42 -0400

    add cross origin

--- src/main/java/com/example/testjpabuddy/donationItem/DonationItemController.java
+++ src/main/java/com/example/testjpabuddy/donationItem/DonationItemController.java
@@ -1,6 +1,7 @@
 package com.example.testjpabuddy.donationItem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class DonationItemController {
 
     DonationItemService donationItemService;
--- src/main/java/com/example/testjpabuddy/eventAgencyJob/EventAgencyJobController.java
+++ src/main/java/com/example/testjpabuddy/eventAgencyJob/EventAgencyJobController.java
@@ -1,12 +1,14 @@
 package com.example.testjpabuddy.eventAgencyJob;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class EventAgencyJobController {
 
     EventAgencyJobService eventAgencyJobService;
